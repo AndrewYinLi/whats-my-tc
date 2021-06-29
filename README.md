99dcbdfe400d17dd0c110426c7d54a7448aa524c
Andrew Li <anli@confluent.io>
2021-06-29 14:43:49 -0700

    nice

---
+++ README.md
@@ -0,0 +1,8 @@
+### To help you stop obsessively googling your stock price and running the numbers while you're working
+
+Configure `numbers_example.yaml` and rename it to `numbers.yaml`.
+
+```
+pip3 install -r requirements.txt
+python3 main.py # I recommend aliasing this :)
+```
--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import bios
 from yahoo_fin import stock_info as si
 
 share_price = si.get_live_price("cflt")
-num_shares = 1.0
 
-print(str(share_price * num_shares))
+numbers = bios.read("numbers.yaml")
+
+num_shares = float(numbers["num_shares"])
+num_sold = float(numbers["num_sold"])
+base_salary = float(numbers["base_salary"])
+
+remaining_grant_value = share_price * (num_shares-num_sold)
+annual_grant_value = remaining_grant_value / 4
+
+print("Current share price: " + str(share_price))
+print("Remaining grant value: " + str(remaining_grant_value))
+print("Annual grant value at current share price: " + str(annual_grant_value))
+print("Annual TC at current share price: " + str(base_salary + annual_grant_value))
