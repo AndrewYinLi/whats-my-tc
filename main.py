3017b95eb8aefc0ef9cc24cd4c8adfb0dd8d5cd3
Andrew Li <anli@confluent.io>
2021-06-29 14:56:54 -0700

    add strike price

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from yahoo_fin import stock_info as si
 
 share_price = si.get_live_price("cflt")
 
-numbers = bios.read("numbers.yaml")
+numbers = bios.read("/Users/anli/git/go/src/github.com/confluentinc/whats-my-tc/numbers.yaml")
 
 num_shares = float(numbers["num_shares"])
 num_sold = float(numbers["num_sold"])
+strike_price = float(numbers["strike_price"])
 base_salary = float(numbers["base_salary"])
 
-remaining_grant_value = share_price * (num_shares-num_sold)
+remaining_grant_value = (share_price-strike_price) * (num_shares-num_sold)
 annual_grant_value = remaining_grant_value / 4
 
 print("Current share price: " + str(share_price))
